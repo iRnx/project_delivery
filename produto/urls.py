985b2674a7a5a9af005a43813af1d8fec3737bfa
Renan <renan.rg@hotmail.com.br>
2022-11-26 21:36:37 -0300

    Finalizando parte 1

--- produto/urls.py
+++ produto/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('categoria/<int:id>', views.categoria, name='categoria'),
+    path('produto/<int:id>', views.produto, name='produto'),
 ]
